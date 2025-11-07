5070f1716f00d1fbab25f35a3523ba05588d1091
Xuan Minh <aptmp58@gmail.com>
2025-11-07 21:27:04 +0700

    fix search and filter

--- src/main/java/com/luv2code/jobportal/repository/JobPostActivityRepository.java
+++ src/main/java/com/luv2code/jobportal/repository/JobPostActivityRepository.java
@@ -22,31 +22,51 @@ public interface JobPostActivityRepository extends JpaRepository<JobPostActivity
             " GROUP By j.job_post_id" ,nativeQuery = true)
     List<IRecruiterJobs> getRecruiterJobs(@Param("recruiter") int recruiter);
 
-    @Query(value = "SELECT * FROM job_post_activity j INNER JOIN job_location l on j.job_location_id=l.id  WHERE j" +
-            ".job_title LIKE %:job%"
-            + " AND (l.city LIKE %:location%"
-            + " OR l.country LIKE %:location%"
-            + " OR l.state LIKE %:location%) " +
-            " AND (j.job_type IN(:type)) " +
-            " AND (j.remote IN(:remote)) ", nativeQuery = true)
-    List<JobPostActivity> searchWithoutDate(@Param("job") String job,
-                                            @Param("location") String location,
-                                            @Param("remote") List<String> remote,
-                                            @Param("type") List<String> type);
+    @Query(value = """
+    SELECT * FROM job_post_activity j
+    INNER JOIN job_location l ON j.job_location_id = l.id
+    WHERE
+        (:job IS NULL OR j.job_title LIKE %:job%)
+        AND (
+            :location IS NULL OR
+            l.city LIKE %:location% OR
+            l.country LIKE %:location% OR
+            l.state LIKE %:location%
+        )
+        AND (j.job_type IN(:type))
+        AND (j.remote IN(:remote))
+    """, nativeQuery = true)
+    List<JobPostActivity> searchWithoutDate(
+            @Param("job") String job,
+            @Param("location") String location,
+            @Param("remote") List<String> remote,
+            @Param("type") List<String> type
+    );
 
-    @Query(value = "SELECT * FROM job_post_activity j INNER JOIN job_location l on j.job_location_id=l.id  WHERE j" +
-            ".job_title LIKE %:job%"
-            + " AND (l.city LIKE %:location%"
-            + " OR l.country LIKE %:location%"
-            + " OR l.state LIKE %:location%) " +
-            " AND (j.job_type IN(:type)) " +
-            " AND (j.remote IN(:remote)) " +
-            " AND (posted_date >= :date)", nativeQuery = true)
-    List<JobPostActivity> search(@Param("job") String job,
-                                 @Param("location") String location,
-                                 @Param("remote") List<String> remote,
-                                 @Param("type") List<String> type,
-                                 @Param("date") LocalDate searchDate);
+
+    @Query(value = """
+    SELECT * FROM job_post_activity j
+    INNER JOIN job_location l ON j.job_location_id = l.id
+    WHERE
+        (:job IS NULL OR j.job_title LIKE %:job%)
+        AND (
+            :location IS NULL OR
+            l.city LIKE %:location% OR
+            l.country LIKE %:location% OR
+            l.state LIKE %:location%
+        )
+        AND (j.job_type IN(:type))
+        AND (j.remote IN(:remote))
+        AND (posted_date >= :date)
+    """, nativeQuery = true)
+    List<JobPostActivity> search(
+            @Param("job") String job,
+            @Param("location") String location,
+            @Param("remote") List<String> remote,
+            @Param("type") List<String> type,
+            @Param("date") LocalDate searchDate
+    );
+
     @Query("SELECT COUNT(j) FROM JobPostActivity j WHERE DATE(j.postedDate) = CURRENT_DATE")
     long countTodayJobPosts();
 
